634f36e26cc6a3957e680f3aa997050a05812fb1
dancingkimDH <kimdonghyun0701@gmail.com>
2023-05-25 13:54:59 +0900

    first commit

---
+++ src/Complex.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Complex {
+
+public static void main(String[] args) {
+
+    Scanner myObj = new Scanner(System.in);
+
+
+String [][] polls = {
+{"1. 교수는 수업 전 강의 목표를 명확히 제시하였습니까?"},
+{"전혀 아니다", "아니다", "그렇다", "매우 그렇다"},
+{"2. 강의의 내용은 체계적이고 성의있게 구성되었는가?"},
+{"전혀 아니다", "아니다", "그렇다", "매우 그렇다"},
+{"3. 교수는 강의 내용에 대해 전문적 지식이 있었는가?"},
+{"전혀 아니다", "아니다", "그렇다", "매우 그렇다"},
+{"4. 강의 진행 속도는 적절하였는가?"},
+{"전혀 아니다", "아니다", "그렇다", "매우 그렇다"},
+// polls end
+};
+
+for (int a = 0; a < polls.length; a = a + 2) {
+    for() {
+
+
+    }
+
+
+}
+// main end
+}
+
+}
