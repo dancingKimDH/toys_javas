45ce8027123b1a6b5e168b8f758b8b9c2219a45d
BongWonHee <leeban1004@gmail.com>
2023-05-31 13:15:13 +0900

    try catch 추가 작업

--- src/MaxMins.java
+++ src/MaxMins.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
+
 //최솟값을 구합니다
 public class MaxMins {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-        //변수 설정
-        int A = myObj.nextInt();
-        int B = myObj.nextInt();
-        int resultmax = 0;
-        int resultmin = 0;
-        //최솟값 결과 산출하는 조건문 작성
-        //최댓값 결과 산출하는 조건문 추가 
-        if (A > B) {
-            resultmin = B;
-            resultmax = A;
-        } else {
-            resultmin = A;
-            resultmax = B;
-        }
-        //조건문 출력
-        System.out.println("최솟값은 " + resultmin + "입니다.");
-        System.out.println("최댓값은 " + resultmax + "입니다.");
+        try {
+            Scanner myObj = new Scanner(System.in);
+            // 변수 설정
+            int A = myObj.nextInt();
+            int B = myObj.nextInt();
+            int resultmax = 0;
+            int resultmin = 0;
+            // 최솟값 결과 산출하는 조건문 작성
+            // 최댓값 결과 산출하는 조건문 추가
+            if (A > B) {
+                resultmin = B;
+                resultmax = A;
+            } else {
+                resultmin = A;
+                resultmax = B;
+            }
+            // 조건문 출력
+            System.out.println("최솟값은 " + resultmin + "입니다.");
+            System.out.println("최댓값은 " + resultmax + "입니다.");
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        } // return 0;
+
     }
 }
